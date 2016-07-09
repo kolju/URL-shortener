119c6694c40e9333416b5ff1c223f513cc128e90
kolju <vkolju@ya.ru>
2016-07-09 22:32:47 +0500

    Creating DB model

--- url_shortener/apps.py
+++ url_shortener/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class UrlShtnConfig(AppConfig):
-    name = 'url_shtn'
+class UrlShortenerConfig(AppConfig):
+    name = 'url_shortener'
--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    long_url = models.URLField()
+    short = models.CharField(max_length=20)
+    created = models.DateTimeField()
+    clicks_count = models.PositiveIntegerField()
