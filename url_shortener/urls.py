e13a82c32c14a881851bbc860378cddf723e41b1
kolju <vkolju@ya.ru>
2016-07-13 15:30:07 +0500

    List of all objects with delete option

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^delete/(?P<link_id>[0-9]+)/$', views.delete_obj, name='delete_obj'),
+    url(r'^overall/$', views.overall, name='overall'),
     url(r'^info/(?P<link_id>[0-9]+)/$', views.info, name='info'),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'(?P<short>[0-9A-Za-z]+)/$', views.url_redirect, name='url_redirect')
 ]
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import F
 from url_shortener.forms import LinkShortenerForm
 from .models import Link
 
@@ -20,3 +21,20 @@ def info(request, link_id):
     url = Link.objects.get(id=link_id)
     return render(request, 'url_shortener/info.html', {'url': url})
 
+
+def overall(request):
+    objects_list = Link.objects.all()
+    return render(request, 'url_shortener/overall.html', {'objects_list': objects_list})
+
+
+def url_redirect(request, short):
+    obj = Link.objects.get(short=short)
+    obj.clicks_count = F('clicks_count') + 1
+    # obj.refresh_from_db()
+    obj.save()
+    return redirect(obj.long_url)
+
+
+def delete_obj(request, link_id):
+    Link.objects.filter(id=link_id).delete()
+    return redirect('/shortener/overall/')
